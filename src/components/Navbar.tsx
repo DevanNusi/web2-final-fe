32fbd7b413055c92f74b27f6d6070e3b6fc74aa7
Devan <2381012@unai.edu>
2025-05-02 01:44:23 +0700

    finished backend

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../utils/AuthProvider";
 
 const Navbar = () => {
-  const { user, logout } = useAuth();
+  const { logout } = useAuth();
   const navigate = useNavigate();
 
   const handleLogout = () => {
